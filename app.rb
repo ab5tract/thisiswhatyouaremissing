880a6baca998e5e427469e14b5d189265790d1e8
Oren Mittman <nihil2501@gmail.com>
2012-10-07 00:55:28 -0400

    trying to make youtubeit client

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'youtube_it'
+
+helpers do
+	def client
+		@client ||= YouTubeIt::Client.new(
+			:username => ENV['YOUTUBE_USERNAME'],
+			:password => ENV['YOUTUBE_PASSWORD'],
+			:dev_key  => ENV['YOUTUBE_DEV_KEY']
+		)
+	end
+end
 
 get '/' do
-	'Hello World'
+	puts client.inspect
 end
