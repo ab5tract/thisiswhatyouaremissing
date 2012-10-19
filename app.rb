e2dfc2927a238e995d34e26da62f40e793c5feb2
Oren Mittman <nihil2501@gmail.com>
2012-10-19 03:35:09 -0400

    add button and dropdown without wiring them. layout

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './patches/patches'
 require 'json'
 require 'geocoder'
 require 'facets/enumerable/compact_map'
+require 'nokogiri'
 
 configure :development do
   require './devenv'
@@ -55,4 +56,12 @@ get '/' do
   @search_words     = JSON.dump search_words
 
   erb :index
+end
+
+get '/test' do
+  doc = Nokogiri::XML File.open('data/countrycodes.xml')
+  doc.xpath('//ISO_3166-1_Entry').each do |child|
+    puts child.at('.//ISO_3166-1_Country_name').text
+  end
+  200
 end
